db133ff8cd430a71b48daee91a3c814e2eff0c98
unknown <samuelvasquez@upeu.edu.pe>
2019-11-14 21:41:48 -0500

    segundo commit

--- bdventas/conexion.php
+++ bdventas/conexion.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$conexion = new PDO("mysql:host=localhost;dbname=bdventas","root","");
+		$conexion = new PDO("mysql:host=localhost;dbname=app_egresados","app_egresados",".egresados-");
 	}catch (Exception $e){
 		echo $e->getMessage();
 
--- bdventas/usuarios/mostrar.php
+++ bdventas/usuarios/mostrar.php
@@ -1,6 +1,6 @@
 <?php
 	require '../conexion.php';
-	$query = $conexion->prepare("SELECT * FROM usuarios WHERE estado = 1");
+	$query = $conexion->prepare("SELECT u.usuario, u.password, p.nombre as perfil FROM usuario u, perfil p WHERE u.id_perfil=p.id_perfil");
 
 	$query->execute();
 	$usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
